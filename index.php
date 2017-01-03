f7a525827d9f4c66be38302ca1ceb452b73062d9
Vivin Lukose <vivinlukose@Vivins-MacBook-Air.local>
2017-01-02 22:34:02 -0800

    resposive

--- index.php
+++ index.php
@@ -39,13 +39,14 @@
 <body id="page-top">
 
     <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand page-scroll" href="#page-top"><img src="img/Logo-small.png"></a>
+                <a class="navbar-brand page-scroll" href="#page-top"><img src="img/Logo-small.png" alt="Logo"></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -66,7 +67,6 @@
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
-        </div>
         <!-- /.container-fluid -->
     </nav>
 
